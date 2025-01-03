a5ed5d7e0677674980c5f62fb8abbe863ebfc3ee
Gilson Souza <br.gilson.sp@gmail.com>
2025-01-02 21:37:18 -0300

    change hour format

--- app/src/main/java/br/org/piba/sporting_event_race/utils/DataTimeFormatterUtils.java
+++ app/src/main/java/br/org/piba/sporting_event_race/utils/DataTimeFormatterUtils.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 public final class DataTimeFormatterUtils {
     public static final DateTimeFormatter FORMATTER_DATE = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public static final DateTimeFormatter FORMATTER_HOUR = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+    public static final DateTimeFormatter FORMATTER_HOUR = DateTimeFormatter.ofPattern("HH:mm:ss.SS");
 
     private DataTimeFormatterUtils(){}
 
